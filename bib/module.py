77d298ae6fe6599d261c02acbeb754773bcec46a
Daniel <87285520+dannielnotfound@users.noreply.github.com>
2023-04-25 22:26:18 -0300

    Modificação das funções
    Conceitos de Granularidade e Funções (Parametros e Return)

--- bib/module.py
+++ bib/module.py
@@ -1,47 +1,110 @@
 from traceback import print_tb
 
 
+# Function 1 - Granulção - Start
+
+def recebe_metros():
+     return float(input("Digite um valor em metros: "))   
+
+def metros_para_centimetros(m):
+    return m * 100
+
 def exercicio_1_function():
-    meters = float(input("Digite um valor em metros: "))  
-    centimenters = meters * 100  
-    print(f"{meters} metros equivalem a {centimenters} centímetros.")  
+    metros = recebe_metros()
+    centimetros = metros_para_centimetros(metros)
+    print(f"{metros} metros equivalem a {centimetros} centímetros.")  
+
+# Funciton 1 - End
+
+# Function 2 - Start
+
+def input_salario_hora():
+    return float(input("Quanto você ganha por hora? (Sómente núnmeros) "))
+
+def input_horas_trabalhadas():
+    return float(input("Quantas horas você trabalha por mês? "))
+
+def calcula_salario(salario_hora, horas_trabalhadas):
+    return salario_hora * horas_trabalhadas
 
 def exercicio_2_function():
-    salario_hora = float(input("Quanto você ganha por hora? (Sómente núnmeros) "))
-    horas_trabalhadas = float(input("Quantas horas você trabalha por mês? "))
-    salario = salario_hora * horas_trabalhadas
+    salario_hora = input_salario_hora()
+    horas_trabalhadas = input_horas_trabalhadas()
+    salario = calcula_salario(salario_hora, horas_trabalhadas)
     print(f"Seu salário mensal é de R$ {salario:.2f}")
 
+# Function 2 - End
+ 
+# Function 3 - Start
+
+def input_fahrenheit():
+    return float(input("Digite a temperatura em graus fahrenheit: "))
+
+def calcula_fahrenheit_para_celsius(fahrenheit):
+    return (5 * (fahrenheit - 32) / 9)
+
 def exercicio_3_function():
-    fahrenheit = float(input("Digite a temperatura em graus fahrenheit: "))
-    celsius = (5 * (fahrenheit - 32) / 9)
+    fahrenheit = input_fahrenheit()
+    celsius = calcula_fahrenheit_para_celsius(fahrenheit)
     print(f"A temperatura em graus Celsius é: {fahrenheit}F em Celsius é {celsius:.2f}")
 
+# Function 3 - End
+
+# Function 4 - Start
+
+def input_altura():
+    return float(input("Minha altura é (m): ")) 
+
+def input_peso():
+    return float(input("Meu peso é de (kg):"))
+
+def calcula_imc(peso, altura):
+    return peso / (altura ** 2)
+
 def exercicio_4_function():
-    alt = float(input("Minha altura é (m): ")) 
-    peso = float(input("meu peso e de (kg)"))
-    imc = peso / (alt ** 2)
+    alt = input_altura()
+    peso = input_peso()
+    imc = calcula_imc(peso, alt)
     print('O IMC desta pessoa é de {:.2f}'.format(imc))
+
+# Function 4 - End 
+
+# Function 5 - Start
+
+def input_peso_peixe():
+    return float(input("Peso total dos peixes (KG): "))
+
+def calcula_peso_excedente(peso, limite):
+    return peso - limite
+
+def calcula_multa(peso_excedente, multa):
+    return peso_excedente * multa
 
 def exercicio_5_function():
     limite = 30
     multa = 3.00
     pesoExcedente = 0
-    peso = float(input("Peso total dos peixes (KG): "))
-    if peso > limite:
-        pesoExcedente = peso - limite
-        multa = pesoExcedente * multa
+    peso_peixe = input_peso_peixe()
+    if peso_peixe > limite:
+        pesoExcedente = calcula_peso_excedente(peso_peixe, limite)
+        multa = calcula_multa(pesoExcedente, multa)
         print("Peso acima de:", limite,"kg. Multado em R$", multa,"pelo excesso de: ", pesoExcedente,"kg.")
     else:
         print("Peso dos peixes dentro do limite. Sem multas para você Zé Papo-de-Pescador")
 
-def exercicio_6_function():
-    salario_hora = float(input("Quantos reais você ganha por hora? "))
-    horasTrabalhadas = float(input("Quantas horas você trabalha por mês? "))
+# Function 5 - End
 
-    salario = salario_hora * horasTrabalhadas
-    imposto = salario * 0.11
-    imp_inss = salario * 0.08
+# Function 6 - Start
+def calcula_imposto(salario, porcentagem):
+    return salario * porcentagem
+
+def exercicio_6_function():
+    salario_hora = input_salario_hora()
+    horasTrabalhadas = input_horas_trabalhadas()
+
+    salario = calcula_salario(salario_hora, horasTrabalhadas)
+    imposto = calcula_imposto(salario, 0.11)
+    imp_inss = calcula_imposto(salario, 0.08)
     descontos = imposto + imp_inss
     salario_liquido = salario - descontos
 
@@ -50,6 +113,7 @@ def exercicio_6_function():
     print("Você pagou R$ {:.2f} de imposto".format(imposto))
     print("Você pagou R$ {:.2f} de INSS".format(imp_inss))
 
+# Function 6 - End
 def exercicio_7_function():
     latas = 80 
     t_latas = 18 
@@ -62,6 +126,12 @@ def exercicio_7_function():
     total = latasUsadas * latas
     print(f"Você precisará de {latasUsadas:.0f} lata(s) de tinta. Total de R$ {total:.2f}.")
 
+
+
+
+
+
+# Teste de retorno
 
 def exercicio_1():
     print('\n')
